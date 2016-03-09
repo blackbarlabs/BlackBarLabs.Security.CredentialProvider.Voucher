26fea3191db8e2c866e685314ce120f7636d20af
joshcodes <josh@blackbarlabs.com>
2016-03-09 11:53:11 -0600

    initial

---
+++ Utilities.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace BlackBarLabs.Security.CredentialProvider.Voucher
+{
+    public static class Utilities
+    {
+        public static Uri GetTrustedProviderId()
+        {
+            var trustedVoucherProviderString = ConfigurationManager.AppSettings["BlackbarLabs.Security.CredentialProvider.Voucher.provider"];
+            var trustedVoucherProviderId = new Uri(trustedVoucherProviderString);
+            return trustedVoucherProviderId;
+        }
+
+        public static string GenerateToken(Guid authId, DateTime validUntilUtc)
+        {
+            byte[] signatureData;
+            var hashedData = ComputeHashData(authId, validUntilUtc, out signatureData);
+
+            var trustedVoucherPrivateKey = RSA.RSAFromConfig("BlackbarLabs.Security.CredentialProvider.Voucher.key");            
+            var signature = trustedVoucherPrivateKey.SignHash(hashedData, CryptoConfig.MapNameToOID("SHA256"));
+
+            var tokenBytes = signatureData.Concat(signature).ToArray();
+            return Convert.ToBase64String(tokenBytes);
+        }
+
+        public static T ValidateToken<T>(string accessToken,
+            Func<Guid, T> success, Func<T> tokenExpired, Func<T> invalidSignature)
+        {
+            #region Parse token
+
+            var tokenBytes = Convert.FromBase64String(accessToken);
+
+            var guidSize = Guid.NewGuid().ToByteArray().Length;
+            var dateTimeSize = sizeof(long);
+
+            var authIdData = tokenBytes.Take(guidSize).ToArray();
+            var validUntilUtcData = tokenBytes.Skip(guidSize).Take(dateTimeSize).ToArray();
+            var validUntilTicks = BitConverter.ToInt64(validUntilUtcData, 0);
+
+            var authId = new Guid(authIdData);
+            var validUntilUtc = new DateTime(validUntilTicks, DateTimeKind.Utc);
+            var providedSignature = tokenBytes.Skip(guidSize + dateTimeSize).ToArray();
+
+            #endregion
+
+            if (validUntilTicks < DateTime.UtcNow.Ticks)
+                return tokenExpired();
+
+            byte[] signatureData;
+            var hashedData = ComputeHashData(authId, validUntilUtc, out signatureData);
+            
+            var trustedVoucher = RSA.RSAFromConfig("BlackbarLabs.Security.CredentialProvider.Voucher.key.pub");
+            if (!trustedVoucher.VerifyHash(hashedData, CryptoConfig.MapNameToOID("SHA256"), providedSignature))
+                return invalidSignature();
+
+            return success(authId);
+        }
+
+        private static byte [] ComputeHashData(Guid authId, DateTime validUntilUtc, out byte [] signatureData)
+        {
+            var authIdData = authId.ToByteArray();
+            var validUntilUtcData = BitConverter.GetBytes(validUntilUtc.Ticks);
+            signatureData = authIdData.Concat(validUntilUtcData).ToArray();
+
+            var hash = new SHA256Managed();
+
+            var hashedData = hash.ComputeHash(signatureData);
+            return hashedData;
+        }
+    }
+}
---
+++ VoucherCredentialProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace BlackBarLabs.Security.CredentialProvider.Voucher
+{
+    public class VoucherCredentialProvider : IProvideCredentials
+    {
+        public async Task<string> RedeemTokenAsync(Uri providerId, string username, string accessToken)
+        {
+            var trustedProvider = Utilities.GetTrustedProviderId();
+            if (String.Compare(providerId.AbsoluteUri, trustedProvider.AbsoluteUri) != 0)
+                return default(string);
+
+            var userNameId = await Task.FromResult(Guid.Parse(username));
+
+            return Utilities.ValidateToken(accessToken,
+                (authId) =>
+                {
+                    if (authId.CompareTo(userNameId) != 0)
+                        return default(string);
+                    return authId.ToString();
+                },
+                () => default(string),
+                () => default(string));
+        }
+    }
+}
